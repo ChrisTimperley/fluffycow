5a90176f107b19f6e73ded080cb8fde60f75dcdd
ChrisTimperley <christimperley@googlemail.com>
2019-03-26 17:48:31 -0400

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+PACKAGE_NAME = 'fluffcow'
+VERSION = '0.0.1'
+REQUIRES = open('requirements.txt').readlines()
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    python_requires='>=3.5',
+    description='A domain-specific language for generating random inputs',
+    long_description=open('README.md').read(),
+    author='Chris Timperley',
+    author_email='ctimperley@cmu.edu',
+    url='https://github.com/ChrisTimperley/fluffycow',
+    license='Apache License 2.0',
+    install_requires=REQUIRES,
+    packages=['fluffycow'],
+    keywords=['random', 'fuzzing', 'dsl'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
